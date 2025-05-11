787befdcfc03e70a119b5f9c6ad33cf39b5496b7
Piccio Hub <201469659+Piccio-Code@users.noreply.github.com>
2025-05-11 15:51:07 +0200

    Implemented the multithreading when scraping the songs from the channel and when downloading the song from the playlist.

--- ChannelScraper.py
+++ ChannelScraper.py
@@ -1,4 +1,7 @@
-from pytubefix import Channel, Playlist
+import time
+from multiprocessing.pool import ThreadPool
+
+from pytubefix import Channel
 import requests
 
 
@@ -17,4 +20,8 @@ class ChannelScraper:
 
 
     def get_playlists_dict(self):
-        return [{"title": playlist.title, "url": playlist.playlist_url, "thumbnail_url": playlist.thumbnail_url} for playlist in self.channel.playlists]
+        playlists = self.channel.playlists
+
+        with ThreadPool(processes=len(playlists)) as pool:
+            result = pool.map(lambda playlist: {"title": playlist.title, "url": playlist.playlist_url, "thumbnail_url": playlist.thumbnail_url}, playlists)
+            return result
--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import time
+from multiprocessing.pool import ThreadPool
+
 from flask import Flask, render_template, session, request, send_file
 import pytubefix
 import os
@@ -17,15 +20,14 @@ def playlist():
         selected_playlist = pytubefix.Playlist(request.form["selected_playlist"]).videos
 
         with tempfile.TemporaryDirectory(prefix="download", ignore_cleanup_errors=True) as download_folder:
-            for video in selected_playlist:
-                ys = video.streams.get_audio_only()
-                ys.download(download_folder + "\\playlist")
 
-            print(os.listdir(download_folder + "\\playlist"))
+            with ThreadPool(processes=8) as pool:
+                pool.map_async(download, ((video, download_folder) for video in selected_playlist)).get()
+
             with zipfile.ZipFile(download_folder + "\\file.zip", "w") as f:
                 for file in os.listdir(download_folder + "\\playlist"):
                     song = download_folder + "\\playlist\\" + file
-                    f.write(song, "\\Playlist" + file.title())
+                    f.write(song, "Playlist\\" + file.title())
 
             return send_file(download_folder + "\\file.zip", as_attachment=True)
 
@@ -40,6 +42,11 @@ def playlist():
 
     return render_template("playlistSelector.html", playlists = playlist_scraper.get_playlists_dict())
 
+def download(info):
+    video: pytubefix.__main__.YouTube = info[0]
+    download_folder = info[1]
+
+    video.streams.get_audio_only().download(download_folder + "\\playlist")
 
 if __name__ == '__main__':
     app.run()
